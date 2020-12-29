a906ce1acc0611a11d867211152eca50742bfe40
The Sandulph <mostafa.rezvani72@gmail.com>
2020-12-29 10:30:24 +0330

    release

--- src/string.js
+++ src/string.js
@@ -1,17 +1,14 @@
-import initialism from 'initialism';
-import * as change_case from 'change-case';
-
-
-export const camelCase = change_case.camelCase;
-export const capitalCase = change_case.capitalCase;
-export const constantCase = change_case.constantCase;
-export const dotCase = change_case.dotCase;
-export const headerCase = change_case.headerCase;
-export const noCase = change_case.noCase;
-export const paramCase = change_case.paramCase;
-export const pascalCase = change_case.pascalCase;
-export const pathCase = change_case.pathCase;
-export const sentenceCase = change_case.sentenceCase;
-export const snakeCase = change_case.snakeCase;
-
-export const abbreviate = initialism;
+export abbreviate from 'initialism';
+export {
+    camelCase,
+    capitalCase,
+    constantCase,
+    dotCase,
+    headerCase,
+    noCase,
+    paramCase,
+    pascalCase,
+    pathCase,
+    sentenceCase,
+    snakeCase,
+} from 'change-case';
